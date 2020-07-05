5b8aba6754599bc74e4c12c511187f479e5c5353
M Groesbeck <m.groesbeck@gmail.com>
2020-07-05 16:27:20 -0700

    Routers, models and migrations complete.

--- api/server.js
+++ api/server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const cors = require('cors');
 const helmet = require('helmet');
 
+const usersRouter = require("./users/users-router");
+const solutionsRouter = require("./solutions/solutions-router");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/api/users", usersRouter);
+server.use("/api/solutions", solutionsRouter);
+
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
 });
--- api/solutions/solutions-router.js
+++ api/solutions/solutions-router.js
@@ -11,6 +11,40 @@ router.get("/all", (req, res) => {
         });
 });
 
-router.post("/:id", authenticator, (req, res) => {
-    
+router.post("/:solution", authenticator, (req, res) => {
+    const thisSolution = req.params.solution;
+    const validity = Solutions.checkSolution(thisSolution);
+    if (!validity) {
+        res.status(200).json({ message: "Invalid solution." });
+    } else {
+        Solutions.findThis(thisSolution)
+            .then((prev) => {
+                if(prev){
+                    res.status(200).json({
+                        message: "Solution already found.",
+                        solution: prev
+                    });
+                } else {
+                    if (req.decodedToken){
+                        Solutions.add(thisSolution)
+                        .then((sol) => {
+                            res.status(200).json({
+                                message: "New solution saved.",
+                                solution: sol
+                            });
+                        })
+                        .catch((error) => {
+                            res.status(500).json({ errorMessage: error.message });
+                        });
+                    } else {
+                        res.status(200).json({
+                            message: "Valid solution; log in to save."
+                        });
+                    }
+                }
+            })
+            .catch((error) => {
+                res.status(500).json({ errorMessage: error.message });
+            });
+    }
 })
